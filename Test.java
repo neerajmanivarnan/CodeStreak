488a160947dd46dcdbb4b51023263cb687df1d41
neerajmanivarnan <neerajmanivarnan666@gmail.com>
2024-02-11 22:23:21 +0530

    testfiles

--- Test.java
+++ Test.java
@@ -3,8 +3,15 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.next();
+        
+        int[] arr = {1,2,3,1,2};
+        int result = 0;
+        for(int i=0;i<arr.length;i++){
+            result = result ^ arr[i];
+        }
 
-        System.out.println(name);
+        System.out.println(result);
+
+        
     }
 }
--- fancyNumSol2.java
+++ fancyNumSol2.java
@@ -5,25 +5,25 @@ public class fancyNumSol2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Get the number of numbers
+       
         int numNumbers = scanner.nextInt();
 
-        // Count the fancy numbers and store them
+       
         int fancyCount = 0;
         int[] fancyNumbers = new int[numNumbers];
 
-        // Iterate over each number
+    
         for (int i = 0; i < numNumbers; i++) {
             int number = scanner.nextInt();
 
-            // Check if the number is fancy
+            
             if (isFancyNumber(number)) {
                 fancyNumbers[fancyCount++] = number;
             }
         }
 
-        // Print the results
-        System.out.println("Total fancy numbers: " + fancyCount);
+       
+        System.out.println(fancyCount);
         for (int i = 0; i < fancyCount; i++) {
             System.out.println(fancyNumbers[i]);
         }
@@ -31,19 +31,19 @@ public class fancyNumSol2 {
         scanner.close();
     }
 
-    // Helper function to check if a number is fancy
+  
     public static boolean isFancyNumber(int number) {
         int numDigits = String.valueOf(number).length();
 
-        // Get the first and last digits
+        
         int firstDigit = number / (int) Math.pow(10, numDigits - 1);
         int lastDigit = number % 10;
 
-        // Return true if only first and last digits are non-zero, otherwise false
+        
         return firstDigit != 0 && lastDigit != 0 && isMiddleZero(number, numDigits);
     }
 
-    // Helper function to check if middle digits are all zero
+   
     public static boolean isMiddleZero(int number, int numDigits) {
         for (int i = 1; i < numDigits - 1; i++) {
             if (number / (int) Math.pow(10, numDigits - i - 1) % 10 != 0) {
