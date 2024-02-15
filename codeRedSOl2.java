05ab8da2af56c7809a22c0c450bf83cf59aeac9d
neerajmanivarnan <neerajmanivarnan666@gmail.com>
2024-02-15 20:54:20 +0530

    name modifications

--- codeRedSOl2.java
+++ codeRedSOl2.java
@@ -15,7 +15,7 @@ class patient {
     }
 }
 
-public class codeRed {
+public class codeRedSOl2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
--- colorsSol2.java
+++ colorsSol2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class colors {
+public class colorsSol2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
