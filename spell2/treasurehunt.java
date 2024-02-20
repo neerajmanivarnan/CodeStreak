f1d39eb0beedfc38883b81941c52d80ce1f49622
neerajmanivarnan <neerajmanivarnan666@gmail.com>
2024-02-20 12:12:19 +0530

    completed

--- spell2/treasurehunt.java
+++ spell2/treasurehunt.java
@@ -11,6 +11,17 @@ public class treasurehunt{
         for(int i=0;i<num;i++){
             arr[i] = sc.nextInt();
         }
+        int temp;
+
+        // for(int i=0;i<num-1;i++){
+        //     for(int j=0;j<num-i-1;j++){
+        //         if(arr[j]>arr[j+1]){
+        //             temp = arr[j];
+        //             arr[j] = arr[j+1];
+        //             arr[j+1] = temp;
+        //         }
+        //     }
+        // }
 
         int d = findD(arr);
 
@@ -22,7 +33,7 @@ public class treasurehunt{
              result = findMissBT(arr,d);
         }
 
-        
+     
 
        
         
